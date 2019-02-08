f0d74962da701ed9d563f6b6f1316016c71ba28e
Suzanne Witt <switt4@sharcnet.ca>
2019-02-08 14:51:49 -0500

    Update mentalchron_heuristic.py

--- heuristics/mentalchron_heuristic.py
+++ heuristics/mentalchron_heuristic.py
@@ -18,31 +18,27 @@ def infotodict(seqinfo):
     task = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-vma_run-{item:02d}_bold')
     task_sbref = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-vma_run-{item:02d}_sbref')
     
-    #GRE phase diff 
-    fmap_diff = create_key('{bids_subject_session_dir}/fmap/{bids_subject_session_prefix}_run-{item:02d}_phasediff')
-    fmap_magnitude = create_key('{bids_subject_session_dir}/fmap/{bids_subject_session_prefix}_run-{item:02d}_magnitude')
+    gre_diff = create_key('{bids_subject_session_dir}/fmap/{bids_subject_session_prefix}_run-{item:02d}_phasediff')
+    gre_magnitude = create_key('{bids_subject_session_dir}/fmap/{bids_subject_session_prefix}_run-{item:02d}_magnitude')
 
     info[task]=[]
     info[task_sbref]=[]
-    info[fmap_diff]=[]
-    info[fmap_magnitude]=[]
+    info[gre_diff]=[]
+    info[gre_magnitude]=[]
 
     for idx, s in enumerate(seqinfo):
        
         if ('bold' in (s.series_description).strip()):
-            if (s.dim4==1 and  'SBRef' in (s.series_description).strip()):
+            if (s.dim4==1 and 'SBRef' in (s.series_description).strip()):
                 info[task_sbref].append({'item': s.series_id})
             elif (s.dim4==3000):
                 info[task].append({'item': s.series_id})
                 
-        #gre field map   
         if ('field_mapping' in s.protocol_name):   
-            if (s.dim4 == 1) and ('gre_field_mapping' == (s.series_description).strip()):
+            if (s.dim4==1 and 'gre_field_mapping' in (s.series_description).strip()):
                 if('P' in (s.image_type[2].strip()) ):
-                    info[fmap_diff].append({'item': s.series_id})
+                    info[gre_diff].append({'item': s.series_id})
                 if('M' in (s.image_type[2].strip()) ):
-                    info[fmap_magnitude].append({'item': s.series_id})
+                    info[gre_magnitude].append({'item': s.series_id})
                     
-
-
     return info
