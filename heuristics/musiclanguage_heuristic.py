0e13dc6ad3e2add4a14121edda774e73e5afdb3f
Suzanne Witt <switt4@sharcnet.ca>
2019-02-26 10:40:18 -0500

    Add new file

---
+++ heuristics/musiclanguage_heuristic.py
@@ -0,0 +1,27 @@
+import os
+import numpy
+from cfmm_base import infotodict as cfmminfodict
+from cfmm_base import create_key
+
+def infotodict(seqinfo):
+    """Heuristic evaluator for determining which runs belong where
+    allowed template fields - follow python string module:
+    item: index within category
+    subject: participant id
+    seqitem: run number during scanning
+    subindex: sub index within group
+    """
+
+    # call cfmm for general labelling and get dictionary
+    info = cfmminfodict(seqinfo)
+
+    task = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-SpeechSongRating_run-{item:02d}_bold')
+
+    info[task]=[]
+
+    for idx, s in enumerate(seqinfo):
+       
+        if ('Run' in (s.series_description).strip() && s.dim4>100):
+            info[task].append({'item': s.series_id})
+
+    return info
