7f705ad8df5b966ebe380c8149eeba34f50b1c87
Suzanne Witt <switt4@sharcnet.ca>
2019-05-01 12:59:52 -0400

    Add new file

---
+++ heuristics/sshrc_mindful_heuristic.py
@@ -0,0 +1,35 @@
+import os
+import numpy
+from cfmm_base import infotodict as cfmminfodict
+from cfmm_base import create_key
+
+def infotodict(seqinfo):
+    """Heuristic evaluator for determining which runs belong where
+    allowed template fields - follow python string module:
+    item: index within category
+    subject: participant id
+    seqitem: run number during scanning
+    subindex: sub index within group
+    """
+
+    # call cfmm for general labelling and get dictionary
+    info = cfmminfodict(seqinfo)
+
+    soundcheck = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-soundcheck_run-{item:02d}_bold')
+
+    mindful = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-mindful_run-{item:02d}_bold')
+    
+    info[soundcheck]=[]
+    info[mindful]=[]
+
+    for idx, s in enumerate(seqinfo):
+       
+        if ('soundcheck' in (s.series_description).strip()):
+            if (s.dim4>1):
+                    info[soundcheck].append({'item': s.series_id})
+                    
+        elif ('mindful' in (s.series_description).strip()):
+            if (s.dim4>1):
+                    info[mindful].append({'item': s.series_id})            
+                    
+    return info
