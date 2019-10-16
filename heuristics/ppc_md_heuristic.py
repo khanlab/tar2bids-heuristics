07b0cbc93ad5abc6e34fadb8cc1b8179544160c1
Suzanne Witt <switt4@sharcnet.ca>
2019-10-16 15:12:45 -0400

    Update ppc_md_heuristic.py

--- heuristics/ppc_md_heuristic.py
+++ heuristics/ppc_md_heuristic.py
@@ -18,14 +18,14 @@ def infotodict(seqinfo):
     info = cfmminfodict(seqinfo)
 
     task_localizer = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-localizer_run-{item:02d}_bold')
-    task_lifetime = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-lifetime_run-{item:02d}_bold')
+    task_test = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-test_run-{item:02d}_bold')
     task_localizerReverse = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-localizerReverse_run-{item:02d}_bold')
     task_localizerTE25 = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-localizerTE25_run-{item:02d}_bold')
     task_study = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-study_run-{item:02d}_bold')
     task_keyprac = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-keyprac_run-{item:02d}_bold')
 
     info[task_localizer]=[]
-    info[task_lifetime]=[]
+    info[task_test]=[]
     info[task_localizerReverse]=[]
     info[task_localizerTE25]=[]
     info[task_study] = []
@@ -33,8 +33,8 @@ def infotodict(seqinfo):
 
     for idx, s in enumerate(seqinfo):
 
-        if ('lifetime' in (s.series_description).strip() and s.dim4>150):
-            info[task_lifetime].append({'item': s.series_id})
+        if ('test' in (s.series_description).strip() and s.dim4>150):
+            info[task_test].append({'item': s.series_id})
 
         elif ('PRC_localizer' in (s.series_description).strip() and 'reverse' not in (s.series_description).strip() and 'TE25' not in (s.series_description).strip()):
             info[task_localizer].append({'item': s.series_id})
