f401600d55c5e59c2ebebd5f43cd98f23f9b6383
Suzanne Witt <switt4@sharcnet.ca>
2021-03-01 14:15:25 -0500

    Update topsy2_heuristic.py

--- heuristics/topsy2_heuristic.py
+++ heuristics/topsy2_heuristic.py
@@ -37,7 +37,7 @@ def infotodict(seqinfo):
                 else:
                     info[pa].append({'item': s.series_id,'dir': 'PA'})
 
-            elif:
+            else:
                 if (s.dim4==1):
                     if 'SBRef' in (s.series_description).strip():
                         info[rest_sbref].append({'item': s.series_id,'dir': 'AP'})
