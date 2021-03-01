648b41ee44b750d72d5f5c24db855772a09fc45d
Suzanne Witt <switt4@sharcnet.ca>
2021-03-01 14:11:58 -0500

    Add new file

---
+++ heuristics/topsy2_heuristic.py
@@ -0,0 +1,48 @@
+import os
+import numpy
+from cfmm_base import infotodict as cfmminfodict
+from cfmm_base import create_key
+
+def infotodict(seqinfo):
+    """Heuristic evaluator for determining which runs belong where
+    allowed template fields - follow python string module:
+    item: index within category
+    subject: participant id
+    seqitem: run number during scanning
+    subindex: sub index within group
+    """
+
+    # call cfmm for general labelling and get dictionary
+    info = cfmminfodict(seqinfo)
+
+    rest = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-rest_dir-{dir}_run-{item:02d}_bold')
+    rest_sbref = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-rest_dir-{dir}_run-{item:02d}_sbref')
+
+    pa = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-rest_dir-{dir}_run-{item:02d}_bold')
+    pa_sbref = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-rest_dir-{dir}_run-{item:02d}_sbref')
+
+    
+
+    info[rest]=[]
+    info[rest_sbref]=[]
+    info[pa]=[]
+    info[pa_sbref]=[]
+
+    for idx, s in enumerate(seqinfo):
+       
+        if ('bold' in (s.series_description).strip()):           
+            if ('PA' in (s.series_description).strip()):
+                if 'SBRef' in (s.series_description).strip():
+                    info[pa_sbref].append({'item': s.series_id,'dir': 'PA'})
+                else:
+                    info[pa].append({'item': s.series_id,'dir': 'PA'})
+
+            if ('test' in (s.series_description).strip()):
+                if (s.dim4==1):
+                    if 'SBRef' in (s.series_description).strip():
+                        info[rest_sbref].append({'item': s.series_id,'dir': 'AP'})
+                    else:
+                        info[rest].append({'item': s.series_id,'dir': 'AP'})
+
+
+    return info
