5bff70b6930e4cf372515e1046476928916cce3f
Suzanne Witt <switt4@sharcnet.ca>
2021-09-09 08:57:06 -0400

    Update mcc_heuristic.py

--- heuristics/mcc_heuristic.py
+++ heuristics/mcc_heuristic.py
@@ -36,7 +36,7 @@ def infotodict(seqinfo):
         if ('bold' in (s.series_description).strip()):
             if (s.dim4==1 and 'SBRef' in (s.series_description).strip()):
                 info[task_sbref].append({'item': s.series_id})
-            elif (s.dim4=328):
+            elif (s.dim4==328):
                 info[task].append({'item': s.series_id})
                 
         if ('field_mapping' in s.protocol_name):   
