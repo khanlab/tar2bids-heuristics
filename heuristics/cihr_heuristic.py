35c0852bcfe50f218d0dfb2533782542cbcaca14
Suzanne Witt <switt4@sharcnet.ca>
2021-10-21 13:11:17 -0400

    Update cihr_heuristic.py

--- heuristics/cihr_heuristic.py
+++ heuristics/cihr_heuristic.py
@@ -17,16 +17,42 @@ def infotodict(seqinfo):
 
     humour = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-humour_run-{item:02d}_bold')
     seinfeld = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-seinfeld_run-{item:02d}_bold')
+    hitchcock = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-hitchcock_run-{item:02d}_bold')
+
+    aspire_mag_echo_GRE = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_acq-ASPIRE_part-mag_echo_GRE')
+    aspire_phase_echo_GRE = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_acq-ASPIRE_part-phase_echo_GRE')
+    aspire_T2_star_GRE = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_acq-ASPIRE_T2star')
+    aspire_R2_star_GRE = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_acq-ASPIRE_R2star')
 
     info[humour]=[]
     info[seinfeld]=[]
+    info[hitchcock]=[]
+
+    info[aspire_mag_echo_GRE] = []
+    info[aspire_phase_echo_GRE] = []
+    info[aspire_T2_star_GRE] = []
+    info[aspire_R2_star_GRE] = []
 
     for idx, s in enumerate(seqinfo):
-       
-        if ('Humour' in (s.series_description).strip()):
+
+        if ('ASPIRE' in (s.series_description).strip()):
+            if ('R2star' in (s.series_description).strip()):
+                info[aspire_R2_star_GRE].append({'item': s.series_id})
+            elif ('TSstar' in (s.series_description).strip()):
+                info[aspire_T2_star_GRE].append({'item': s.series_id})
+            elif len(s.image_type) > 3:
+                if (('M' in (s.image_type[2].strip())) and ('ASPIRE' not in s.image_type)):
+                    info[aspire_mag_echo_GRE].append({'item': s.series_id})
+                if ('P' in (s.image_type[2].strip()) and len(s.image_type) > 4):
+                    info[aspire_phase_echo_GRE].append({'item': s.series_id})            
+
+        elif ('Humour' in (s.series_description).strip()):
             info[humour].append({'item': s.series_id})
                     
         elif ('Seinfeld' in (s.series_description).strip()):
-            info[seinfeld].append({'item': s.series_id})         
+            info[seinfeld].append({'item': s.series_id})   
+
+        elif ('Hitchcock' in (s.series_description).strip()):
+            info[hitchcock].append({'item': s.series_id})       
 
     return info
