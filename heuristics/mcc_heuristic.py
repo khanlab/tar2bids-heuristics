9d7b6eac253ecf051f6342ba89c60ffc1d9f47d7
Suzanne Witt <switt4@sharcnet.ca>
2021-12-21 15:35:52 -0500

    Update mcc_heuristic.py

--- heuristics/mcc_heuristic.py
+++ heuristics/mcc_heuristic.py
@@ -17,9 +17,11 @@ def infotodict(seqinfo):
 
     task = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-task_run-{item:02d}_bold')
     task_sbref = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-task_run-{item:02d}_sbref')
+
+    t2_space = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_acq-SPACE_run-{item:02d}_T2w')
     
-    gre_diff = create_key('{bids_subject_session_dir}/fmap/{bids_subject_session_prefix}_run-{item:02d}_phasediff')
-    gre_magnitude = create_key('{bids_subject_session_dir}/fmap/{bids_subject_session_prefix}_run-{item:02d}_magnitude')
+    #gre_diff = create_key('{bids_subject_session_dir}/fmap/{bids_subject_session_prefix}_run-{item:02d}_phasediff')
+    #gre_magnitude = create_key('{bids_subject_session_dir}/fmap/{bids_subject_session_prefix}_run-{item:02d}_magnitude')
     
     fmap_diff = create_key('{bids_subject_session_dir}/fmap/{bids_subject_session_prefix}_phasediff')
     fmap_magnitude = create_key('{bids_subject_session_dir}/fmap/{bids_subject_session_prefix}_magnitude')
@@ -28,8 +30,10 @@ def infotodict(seqinfo):
 
     info[task]=[]
     info[task_sbref]=[]
-    info[gre_diff]=[]
-    info[gre_magnitude]=[]
+    #info[gre_diff]=[]
+    #info[gre_magnitude]=[]
+    info[t2_space] = []
+
 
     for idx, s in enumerate(seqinfo):
        
@@ -39,11 +43,14 @@ def infotodict(seqinfo):
             elif (s.dim4==328):
                 info[task].append({'item': s.series_id})
                 
-        if ('field_mapping' in s.protocol_name):   
-            if (s.dim4==1 and 'gre_field_mapping' in (s.series_description).strip()):
-                if('P' in (s.image_type[2].strip()) ):
-                    info[gre_diff].append({'item': s.series_id})
-                if('M' in (s.image_type[2].strip()) ):
-                    info[gre_magnitude].append({'item': s.series_id})
+        #elif ('field_mapping' in s.protocol_name):   
+        #    if (s.dim4==1 and 'gre_field_mapping' in (s.series_description).strip()):
+        #        if('P' in (s.image_type[2].strip()) ):
+        #            info[gre_diff].append({'item': s.series_id})
+        #        if('M' in (s.image_type[2].strip()) ):
+        #            info[gre_magnitude].append({'item': s.series_id})
+        
+        elif ('T2_spc_750iso' in s.protocol_name):
+            info[t2_space].append({'item': s.series_id})
                     
     return info
