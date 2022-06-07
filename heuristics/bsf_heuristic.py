ab10b819cbe0c7be2af247593a3932a3e21fed5f
Suzanne Witt <switt4@sharcnet.ca>
2022-06-07 09:23:34 -0400

    Update heuristics/bsf_heuristic.py

--- heuristics/bsf_heuristic.py
+++ heuristics/bsf_heuristic.py
@@ -29,14 +29,14 @@ def infotodict(seqinfo):
        
         if ('foodflanker' in (s.series_description).strip()):
             if (s.dim4==1 and 'SBRef' in (s.series_description).strip()):
-                info[task_sbref].append({'item': s.series_id})
+                info[foodflanker_sbref].append({'item': s.series_id})
             elif (s.dim4>2):
-                info[task].append({'item': s.series_id})
+                info[foodflanker].append({'item': s.series_id})
 
         if ('RSAfood' in (s.series_description).strip()):
             if (s.dim4==1 and 'SBRef' in (s.series_description).strip()):
-                info[task_sbref].append({'item': s.series_id})
+                info[rsafood_sbref].append({'item': s.series_id})
             elif (s.dim4>2):
-                info[task].append({'item': s.series_id})
+                info[rsafood].append({'item': s.series_id})
                     
     return info
