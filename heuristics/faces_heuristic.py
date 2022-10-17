e87c0486cd84a5f1d83fa0fe1b31cdfa7fe05121
Suzanne Witt <switt4@sharcnet.ca>
2022-10-17 12:48:53 -0400

    Update heuristics/faces_heuristic.py

--- heuristics/faces_heuristic.py
+++ heuristics/faces_heuristic.py
@@ -17,10 +17,14 @@ def infotodict(seqinfo):
 
     faces = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-faces_run-{item:02d}_bold')
     localizer = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-localizer_run-{item:02d}_bold')
+    depth = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-DepthLoc_run-{item:02d}_bold')
+    fopsr = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-FOPSRLoc_run-{item:02d}_bold')
     
 
     info[faces]=[]
     info[localizer]=[]
+    info[depth]=[]
+    info[fopsr]=[]
 
     for idx, s in enumerate(seqinfo):
        
@@ -28,7 +32,12 @@ def infotodict(seqinfo):
             info[faces].append({'item': s.series_id})
                     
         elif ('localizer' in (s.series_description).strip()) and (s.dim4>1):
-            info[localizer].append({'item': s.series_id})            
+            if ('Depth' in (s.series_description).strip()):
+                info[depth].append({'item': s.series_id})
+            elif ('FOPSR' in (s.series_description).strip()):
+                info[fopsr].append({'item': s.series_id})
+            else:
+                info[localizer].append({'item': s.series_id})            
         
 
     return info
