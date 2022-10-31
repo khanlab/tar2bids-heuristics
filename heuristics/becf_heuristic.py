e462779c3c17835170e37982a33b0dff35f03d32
Suzanne Witt <switt4@sharcnet.ca>
2022-10-31 11:13:11 -0400

    Add new file

---
+++ heuristics/becf_heuristic.py
@@ -0,0 +1,72 @@
+import os
+import numpy
+from cfmm_base import infotodict as cfmminfodict
+from cfmm_base import create_key
+
+def infotodict(seqinfo):
+    """Heuristic evaluator for determining which runs belong where
+    allowed template fields - follow python string module:
+    item: index within category
+    subject: participant id
+    seqitem: run number during scanning
+    subindex: sub index within group
+    """
+
+    # call cfmm for general labelling and get dictionary
+    info = cfmminfodict(seqinfo)
+
+    matrix = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-matrix_run-{item:02d}_bold')
+    matrix_sbref = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-matrix_run-{item:02d}_sbref')
+
+    endback = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-endback_run-{item:02d}_bold')
+    endback_sbref = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-endback_run-{item:02d}_sbref')
+
+    rest = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-rest_run-{item:02d}_bold')
+    rest_sbref = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-rest_run-{item:02d}_sbref')
+
+    fmap_sbref = create_key('{bids_subject_session_dir}/fmap/{bids_subject_session_prefix}_dir-{dir}_epi')
+
+    t13d = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_acq-MPRAGE_run-{item:02d}_T1w')
+
+    
+
+    info[matrix]=[]
+    info[matrix_sbref]=[]
+    info[endback]=[]
+    info[endback_sbref]=[]
+    info[rest]=[]
+    info[rest_sbref]=[]
+    info[fmap_sbref]=[]
+    info[t13d]=[]
+
+    for idx, s in enumerate(seqinfo):
+       
+        if ('matrix' in (s.series_description).strip()):
+            if (s.dim4==1 and  'SBRef' in (s.series_description).strip()):
+                    info[perception_sbref].append({'item': s.series_id})
+            elif (s.dim4>100):
+                    info[perception].append({'item': s.series_id})
+                    
+        elif ('endback' in (s.series_description).strip()):
+            if (s.dim4==1 and  'SBRef' in (s.series_description).strip()):
+                    info[localizer_sbref].append({'item': s.series_id})
+            elif (s.dim4>100):
+                    info[localizer].append({'item': s.series_id})            
+        
+        elif ('rest' in (s.series_description).strip()):
+            if (s.dim4==1 and 'SBRef' in (s.series_description).strip()):
+                    info[visual_sbref].append({'item': s.series_id})
+            elif (s.dim4>100):
+                    info[visual].append({'item': s.series_id})
+        
+        elif ('PA' in (s.series_description).strip()):
+            if (s.dim4==1):
+                #if 'SBRef' in (s.series_description).strip():
+                info[fmap_sbref].append({'item': s.series_id,'dir': 'PA'})
+
+        elif ('T1_3D' in (s.series_description).strip()):
+            info[t13d].append({'item': s.series_id})        
+
+
+    return info
+
