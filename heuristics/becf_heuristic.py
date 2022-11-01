2fd54b215f5dcdb01f4a7a81e3c688d7e2c41029
Suzanne Witt <switt4@sharcnet.ca>
2022-11-01 16:40:29 -0400

    Update heuristics/becf_heuristic.py

--- heuristics/becf_heuristic.py
+++ heuristics/becf_heuristic.py
@@ -69,10 +69,9 @@ def infotodict(seqinfo):
                 #if 'SBRef' in (s.series_description).strip():
                 info[fmap_sbref].append({'item': s.series_id,'dir': 'PA'})
 
-        elif (s.series_description == 'diff_mb3_95dir_b2000_AP'):
-        #elif ('diff_mb3_95dir_b2000_AP' in (s.series_description).strip()):
-        #    if ('FA' not in (s.series_description).strip()):
-            info[dwi_ap].append({'item': s.series_id,'dir': 'AP'})
+        elif ('diff_mb3_95dir_b2000_AP' in (s.series_description).strip()):
+            if (s.dim4==96):
+                info[dwi_ap].append({'item': s.series_id,'dir': 'AP'})
 
         elif ('diff_mb3_b0_PA' in (s.series_description).strip()):
             info[dwi_pa].append({'item': s.series_id,'dir': 'PA'})
