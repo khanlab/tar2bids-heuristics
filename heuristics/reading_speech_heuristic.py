5c5f1f5c1942854e9873fb99a688491cd34c004f
Suzanne Witt <switt4@sharcnet.ca>
2023-01-10 10:10:01 -0500

    Update heuristics/reading_speech_heuristic.py

--- heuristics/reading_speech_heuristic.py
+++ heuristics/reading_speech_heuristic.py
@@ -13,16 +13,22 @@ def infotodict(seqinfo):
     """
 
     # call cfmm for general labelling and get dictionary
-    info = cfmminfodict(seqinfo)
+    #info = cfmminfodict(seqinfo)
 
     task = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-task_run-{item:02d}_bold')
     task_sbref = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-task_run-{item:02d}_sbref')
 
     fmap_sbref = create_key('{bids_subject_session_dir}/fmap/{bids_subject_session_prefix}_acq-EPI_dir-{dir}_epi')
 
-    info[task]=[]
-    info[task_sbref]=[]
-    info[fmap_sbref]=[]
+    dwi_ap = create_key('{bids_subject_session_dir}/dwi/{bids_subject_session_prefix}_run-{item:02}_dwi')
+    dwi_pa = create_key('{bids_subject_session_dir}/fmap/{bids_subject_session_prefix}_acq-DWI_dir-{dir}_epi')
+
+    t13d = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_acq-MPRAGE_run-{item:02d}_T1w')
+
+    fmap_diff = create_key('{bids_subject_session_dir}/fmap/{bids_subject_session_prefix}_phasediff')
+    fmap_magnitude = create_key('{bids_subject_session_dir}/fmap/{bids_subject_session_prefix}_magnitude')
+
+    info = {task:[], task_sbref:[], fmap_sbref:[], dwi_ap:[], dwi_pa:[], t13d:[], fmap_diff:[], fmap_magnitude:[]}
 
     for idx, s in enumerate(seqinfo):
        
@@ -33,7 +39,17 @@ def infotodict(seqinfo):
                     info[task].append({'item': s.series_id})
         
         elif ('ep_bold_mb4_PA' in (s.series_description).strip()):
-            info[fmap_sbref].append({'item': s.series_id,'dir': 'PA'})              
+            info[fmap_sbref].append({'item': s.series_id,'dir': 'PA'}) 
+
+        elif ('ep2d_diff_mb2_p2_64dirs' in (s.series_description).strip()):
+            if (s.dim4==64):
+                info[dwi_ap].append({'item': s.series_id})
+            
+        elif ('ep2d_diff_mb2_p2_64dirs_PA' in (s.series_description).strip()):
+            info[dwi_pa].append({'item': s.series_id,'dir': 'PA'})
+
+        elif ('MPRAGE' in (s.series_description).strip()):
+            info[t13d].append({'item': s.series_id}) 
 
 
     return info
