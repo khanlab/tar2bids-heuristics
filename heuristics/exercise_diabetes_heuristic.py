dac1a8667fb630fa01c985160eafb54f5ade7653
Suzanne Witt <switt4@sharcnet.ca>
2023-05-11 14:53:02 -0400

    fix mempgrage scans in heuristic

--- heuristics/exercise_diabetes_heuristic.py
+++ heuristics/exercise_diabetes_heuristic.py
@@ -28,15 +28,16 @@ def infotodict(seqinfo):
     #aspire_R2_star_GRE = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_acq-ASPIRE_R2star')
 
     # T1w and T2w
-    t1w = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_acq-tfl3dRMS_run-{item}_T1w')
-    t1w_me = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_acq-tfl3d_run-{item}_echo_MEMPRAGE')
-    t1w_norm = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_acq-tfl3dRMS_run-{item}_T1w')
-    t1w_me_norm = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_acq-tfl3d_run-{item}_echo_MEMPRAGE')
-    t1w_vnavs = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_acq-ABCD3d1_run-{item}_vNav')
+    t1w = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_acq-MPRvNav4eRMS_run-{item}_T1w')
+    t1w_me = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_acq-MPRvNav4e_run-{item}_MEMPRAGE')
+    t1w_norm = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_acq-MPRvNavNorm4eRMS_run-{item}_T1w')
+    t1w_me_norm = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_acq-MPRvNavNorm4e_run-{item}_MEMPRAGE')
+    t1w_vnavs = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_acq-MPRvNav_run-{item}_vNav')
 
     t2w = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_acq-SPCvNavRMS_run-{item}_T2w')
     t2w_norm = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_acq-SPCvNavNormRMS_run-{item}_T2w')
-    t2w_vnavs = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_acq-ABCD3d1_run-{item}_vNav')
+    t2w_vnavs = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_acq-SPCvNav_run-{item}_vNav')
+    t2w_hipp = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_acq-HighResHippocampus_run-{item}_T2w')
 
     #t1w_basic = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_acq-MPRAGE_run-{item}_T1w')
     #t2w_basic = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_acq-SPACE_run-{item}_T2w')
@@ -52,7 +53,7 @@ def infotodict(seqinfo):
 
     info = {rest:[],rest_sbref:[],rest_pa_sbref:[],rest_pa:[],
         t1w_me:[],t1w_me_norm:[],t1w_vnavs:[],t1w:[],
-        t1w_norm:[],t2w_vnavs:[],t2w:[],t2w_norm:[],
+        t1w_norm:[],t2w_vnavs:[],t2w:[],t2w_norm:[],t2w_hipp:[],
         dwi:[],dwi_pa:[],fmap_diff:[],fmap_magnitude:[]}
 
     for idx, s in enumerate(seqinfo):
@@ -104,6 +105,8 @@ def infotodict(seqinfo):
 #                info[t2w_norm].append({'item': s.series_id}) 
             else:
                 info[t2w].append({'item': s.series_id}) 
+        elif ('HighResHippocampus' in s.series_description):
+            info[t2w_hipp].append({'item': s.series_id})
         #elif ('T2w' in s.series_description):
         #    info[t2w_basic].append({'item': s.series_id}) 
 
