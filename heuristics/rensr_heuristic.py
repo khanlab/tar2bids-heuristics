c1740af2467ec83e52ea238e9690ac6b59259401
Suzanne Witt <switt4@uwo.ca>
2023-08-24 20:49:41 -0400

    Update rensr_heuristic.py

--- heuristics/rensr_heuristic.py
+++ heuristics/rensr_heuristic.py
@@ -25,11 +25,11 @@ def infotodict(seqinfo):
     for idx, s in enumerate(seqinfo):
        
         if ('ep_bold_m4' in (s.series_description).strip()):
-            if (s.dim4>100):
-                info[task].append({'item': s.series_id})
-            elif ('PA' in (s.series_description).strip()):
+            if ('PA' in (s.series_description).strip()):
                 info[fmap_sbref].append({'item': s.series_id,'dir': 'PA'})
-
+            elif (s.dim4>1):
+                info[task].append({'item': s.series_id})
+           
     return info
     
 
