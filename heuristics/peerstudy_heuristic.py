d1da9c0a2c92b1744ffa55104fead5b058d07971
Suzanne Witt <switt4@uwo.ca>
2023-11-29 17:13:21 -0500

    Update peerstudy_heuristic.py

--- heuristics/peerstudy_heuristic.py
+++ heuristics/peerstudy_heuristic.py
@@ -21,10 +21,17 @@ def infotodict(seqinfo):
     feedback = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-feedback_run-{item:02d}_bold')
     feedback_sbref = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-feedback_run-{item:02d}_sbref')
 
-    info[rest]=[]
-    info[rest_sbref]=[]
-    info[feedback]=[]
-    info[feedback_sbref]=[]
+    fmap_sbref = create_key('{bids_subject_session_dir}/fmap/{bids_subject_session_prefix}_acq-EPI_dir-{dir}_epi')
+
+    dwi_ap = create_key('{bids_subject_session_dir}/dwi/{bids_subject_session_prefix}_run-{item:02}_dwi')
+    dwi_pa = create_key('{bids_subject_session_dir}/fmap/{bids_subject_session_prefix}_acq-DWI_dir-{dir}_epi')
+
+    t13d = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_acq-MPRAGE_run-{item:02d}_T1w')
+
+    fmap_diff = create_key('{bids_subject_session_dir}/fmap/{bids_subject_session_prefix}_phasediff')
+    fmap_magnitude = create_key('{bids_subject_session_dir}/fmap/{bids_subject_session_prefix}_magnitude')
+
+    info = {rest:[], reset_sbref:[], feedback:[], feedback_sbref:[], fmap_sbref:[], dwi_ap:[], dwi_pa:[], t13d:[], fmap_diff:[], fmap_magnitude:[]}
 
     for idx, s in enumerate(seqinfo):
        
@@ -38,6 +45,26 @@ def infotodict(seqinfo):
             if (s.dim4==1 and  'SBRef' in (s.series_description).strip()):
                     info[feedback_sbref].append({'item': s.series_id})
             elif (s.dim4>700):
-                    info[feedback].append({'item': s.series_id})            
+                    info[feedback].append({'item': s.series_id})  
+
+        elif ('MPRAGE' in (s.series_description).strip()):
+            info[t13d].append({'item': s.series_id}) 
+
+        elif ('field_mapping' in s.protocol_name):   
+            if (s.dim4 == 1) and ('gre_field_mapping' == (s.series_description).strip()):
+                if('P' in (s.image_type[2].strip()) ):
+                    info[fmap_diff].append({'item': s.series_id})
+                if('M' in (s.image_type[2].strip()) ):
+                    info[fmap_magnitude].append({'item': s.series_id})
+
+        elif ('PA' in (s.series_description).strip()):
+            if ('diff' in (s.series_description).strip()):
+                info[dwi_pa].append({'item': s.series_id, 'dir': 'PA'}) 
+
+        elif ('ep2d_diff_mb2_p2_64dirs' in (s.series_description).strip()):
+            if (s.dim4==68):
+                info[dwi_ap].append({'item': s.series_id})
+
+          
 
     return info
